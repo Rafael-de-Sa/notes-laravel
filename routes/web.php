663c75a33b9dbe2b486acc658a2b8fb2a29d69a1
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-06-01 17:08:09 -0300

    :broom: cleanup: cleaning up the project

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,4 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function(){
-   echo "About!";
-});
