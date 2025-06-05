6fb463c65cc1dfcf171def6b8c64258984ec7dcb
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-06-04 23:54:45 -0300

    :sparkles: feat: user and session validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -34,14 +35,39 @@ class AuthController
         $username = $request->input('text_username');
         $password = $request->input('text_password');
 
-        //test database connection
-        try {
-            DB::connection()->getPdo();
-            echo "Connection successful";
-        } catch (\PDOException $e) {
-            echo "Connection failed: " . $e->getMessage();
+        //check if user exists
+        $user = User::where('username', $username)
+            ->where('deleted_at', NULL)
+            ->first();
+
+        if (!$user) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Username ou password incorretos.');
         }
-        echo "End";
+
+        //check if password is correct
+        if (!password_verify($password, $user->password)) {
+            return redirect()
+                ->back()
+                ->withInput()
+                ->with('loginError', 'Username ou password incorretos.');
+        }
+
+        //update last login
+        $user->last_login = date('Y-m-d H:i:s');
+        $user->save();
+
+        //login user
+        session([
+            'user' => [
+                'id' => $user->id,
+                'username' => $user->username
+            ]
+        ]);
+
+        echo "LOGIN COM SUCESSO";
     }
     public function logout()
     {
