580f7f7efef2f8b71b86f58a7ce9b189cf1d9690
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-07-13 10:24:00 -0300

    :sparkles: checkIsLogged middleware

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class MainController
 {
-    //
+
+    public function index()
+    {
+        echo "I'm inside the app!";
+    }
+
+    public function newNote()
+    {
+        echo "I'm creating a new note!";
+    }
 }
